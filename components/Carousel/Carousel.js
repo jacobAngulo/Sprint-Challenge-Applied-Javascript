319ff8ecd2a11986f7821ee9c341aaf887977879
Jacob Angulo <jacobangulo@hotmail.com>
2019-02-01 10:21:06 -0800

    carousel!

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,45 @@
 class Carousel {
+    constructor(carousel) {
+        this.carousel = carousel;
+        this.rightButton = this.carousel.querySelector('.right-button');
+        this.leftButton = this.carousel.querySelector('.left-button');
+        this.imgList = this.carousel.querySelectorAll('img');
+        this.currentImageIndex = 0;
+        this.currentImage = this.imgList[this.currentImageIndex];
+        this.currentImage.style.display = 'block';
+        this.rightButton.addEventListener('click', () => this.carouselRight());
+        this.leftButton.addEventListener('click', () => this.carouselLeft());
+    }
 
+    carouselRight(){
+        console.log('testR');
+        this.currentImageIndex++;
+        this.currentImage.style.display = 'none';
+        if(this.currentImageIndex > (this.imgList.length -1)) {
+            this.currentImageIndex = 0;
+            this.currentImage = this.imgList[this.currentImageIndex];
+            this.currentImage.style.display = 'block';    
+        } else {
+            this.currentImage = this.imgList[this.currentImageIndex];
+            this.currentImage.style.display = 'block';    
+        }
+    };
+    carouselLeft(){
+        console.log('testL');
+        this.currentImageIndex--;
+        this.currentImage.style.display = 'none';
+        if(this.currentImageIndex < 0) {
+            this.currentImageIndex = this.imgList.length -1;
+            this.currentImage = this.imgList[this.currentImageIndex];
+            this.currentImage.style.display = 'block';    
+        } else {
+            this.currentImage = this.imgList[this.currentImageIndex];
+            this.currentImage.style.display = 'block';    
+        }
+    };
 }
 
-let carousel = document.querySelector();
+const carousel = document.querySelectorAll('.carousel').forEach((carousel) => new Carousel(carousel));
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
@@ -12,3 +49,4 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+
